568f07a02f5f515153ef3982907084b320deec76
Shradha Kallaje <shradhakallaje@Shradhas-MacBook-Pro.local>
2023-12-31 16:44:56 +0530

    Add user help function and final code cleanup

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
-from decimal import Decimal
+# FIX FORMATTING FUNCTION.
 
-# Defining global variables.
+# Define global variables.
 num1 = 0
 num2 = 0
 result = 'N/A'
 num_format = ''
 valid = True
 memory = []
-
-# Define an 'operators' list (if modified, the 'user_help()' function needs to be updated).
+# Define an 'operators' list (if modified, update the 'user_help()' function).
 operators = ['+', '-', '*', '/', '%', 'A', '**', '//']
 
+
+# Define a 'value-formatting' function (display '.0' floats as integers).
 def format_num(x):
     """
     formatting numbers so that '.0' floating point numbers are displayed as integers
@@ -24,9 +25,10 @@ def format_num(x):
         pass
     return num_format
 
+
 class Operations:
     """
-    Define a class to store operations performaed for memory function
+    A class to store operations performed for memory function
     """
     def __init__(self, value1, operator, value2, answer):
         self.value1 = value1
@@ -35,55 +37,60 @@ class Operations:
         self.answer = answer
 
 
+
 def past_results():
     """
-    Function to list last 10 operations
+    Function to list previous 10 operations
     """
     global memory, num1
     print('\nPrevious operations:'
           '\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
     if len(memory) == 0:
         print('None')
-
     while len(memory) > 10:
         memory.pop(0)
-
-    for mem in memory:
-        print('>', mem.value1, mem.operator, mem.value2, '=', mem.answer)
+    for m in memory:
+        print('>', m.value1, m.operator, m.value2, '=', m.answer)
     print('\nContinue your operation'
           '\nYour first value is ───→', num1)
-    
-    def num_validation(num):
-        """
-        Function to check if the user entered number is other than whole number or decimal
-        """
-        global valid
-        number = ''
-        while valid:
-            try:
-                number = float(input(f"{num}:\n"))
-                break
-            except ValueError:
-                print('\nInvalid input! (Only whole numbers and decimals allowed!).')
-        return number
-    
-    def operator_validation():
-        operator = input('\nOperator:\n').upper()
-        while operator not in operators or operator.isspace():
-            if operator == 'H':
-                user_help()
-                operator = input('\nOperator: ').upper()
-            elif operator == 'M':
-                past_results()
-                operator = input('\nOperator: ').upper()
-            elif operator == 'R':
-                print('\nYour calculation has been reseted.\n')
-                calculate_result()
-            else:
-                print('\n► Invalid operator, try again.\n')
-                operator = input('Operator: ').upper()
-        return operator
-    
+
+
+def num_validation(num):
+    """
+    Function to check if the user entered number is other than whole number or decimal
+    """
+    global valid
+    number = ''
+    while valid:
+        try:
+            number = float(input(num + ': '))
+            break
+        except ValueError:
+            print('\nInvalid input (numbers and decimal points only).')
+    return number
+
+
+
+def operator_validation():
+    """
+    Validate user entered operator
+    """
+    op = input('\nOperator: ').upper()
+    while op not in operators or op.isspace():
+        if op == 'H':
+            user_help()
+            op = input('\nOperator: ').upper()
+        elif op == 'M':
+            past_results()
+            op = input('\nOperator: ').upper()
+        elif op == 'R':
+            print('\nYour calculation has been reseted.\n')
+            calculate()
+        else:
+            print('\n► Invalid operator, try again.\n')
+            op = input('Operator: ').upper()
+    return op
+
 
 def operation_to_perform(op):
     """
@@ -104,10 +111,10 @@ def operation_to_perform(op):
             elif op == '/':  # DIVISION
                 result = float(num1) / float(num2)
                 break
-            elif op == '**':  # EXPONENTIAL
+            elif op == '**':  # POWER
                 result = float(num1) ** float(num2)
                 break
-            elif op == '//':  # SQUARE ROOT
+            elif op == '//':  # ROOT
                 result = float(num1) ** (1 / float(num2))
                 break
             elif op == 'A':  # AVERAGE
@@ -116,10 +123,10 @@ def operation_to_perform(op):
             elif op == '%':  # PERCENTAGE
                 result = float(num1) * float(num2) / 100
                 break
-        except ArithmeticError as e:
-            print('\nError:', e)
+        except ArithmeticError as error:
+            print('\nError:', error)
             break
-    # Adding the operation to the 'memory' list.
+    # Add the operation to the 'memory' list.
     operation = Operations(num1, op, num2, result)
     memory.append(operation)
     # Format and display result.
@@ -128,19 +135,48 @@ def operation_to_perform(op):
 
 
 
-def calculate_result():
+def calculate():
     """calculate the result based on
     the user entered numbers and arithmetic operato
-    r"""
+    r - main program LOOP"""
     global num1, num2, valid
+    while valid:
+        num1 = num_validation('\nFirst value')
+        operator = operator_validation()
+        num2 = num_validation('\nSecond value')
+        operation_to_perform(operator)
 
 
-
+# Define "user_help" function.
 def user_help():
-    """User help options
-    """
     global num1
+    print('\nList of possible operators and their uses:'
+          '\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾'
+          '\n- Use [ M ] to display a list of the last 10 operations.'
+          '\n- Use [ R ] to reset your calculation.'
+          '\n- Use [ + ] for Addition - [Value #1] plus [Value #2].'
+          '\n- Use [ - ] for Subtraction - [Value #1] minus [Value #2].'
+          '\n- Use [ * ] for Multiplication - [Value #1] by [Value #2].'
+          '\n- Use [ / ] for Division - [Value #1] divided by [Value #2].'
+          '\n- Use [ % ] for Percentage - [Value #1] percentage of [Value #2].'
+          '\n- Use [ A ] for Average - Average of [Value #1] and [Value #2].'
+          '\n- Use [ ** ] for Power operation - [Value #1] raised by the power of [Value #2].'
+          '\n- Use [ // ] for Root operation - [Value #1] rooted by the value of [Value #2].'
+          '\n\nContinue your operation'
+          '\nYour first value is ───→',  num1)
 
 
-def main():
-    calculate_result()
+""" 
+START OF PROGRAM EXECUTION 
+"""
+
+# Welcome user and introduce the 'help' operator.
+print('\n         ╔═══════════════════════════════╗'
+      '\n         ║ Welcome to Terminal Calculator║'
+      '\n         ╚═══════════════════════════════╝'
+      '\n\n► If you need help with available operators and their'
+      '\n    uses, enter \'H\' when the operator is requested.'
+      '\n\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
+
+# Execute 'calculate' LOOP.
+calculate()
