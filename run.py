09baff1b518983c58860401d3de5578358399868
Shradha Kallaje <shradhakallaje@Shradhas-MacBook-Pro.local>
2023-12-31 15:46:08 +0530

    Adding menory function

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ operators = ['+', '-', '*', '/', '%', 'A', '**', '//']
 
 def format_num(x):
     """
-    formatting numbers so that floating points are displayed as integers
+    formatting numbers so that '.0' floating point numbers are displayed as integers
     """
     global num_format
     try:
@@ -23,3 +23,29 @@ def format_num(x):
     except ValueError:
         pass
     return num_format
+
+class Operations:
+    """
+    Define a class to store operations performaed for memory function
+    """
+    def __init__(self, value1, operator, value2, answer):
+        self.value1 = value1
+        self.operator = operator
+        self.value2 = value2
+        self.answer = answer
+
+
+def past_results():
+    global memory, num1
+    print('\nPrevious operations:'
+          '\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
+    if len(memory) == 0:
+        print('None')
+
+    while len(memory) > 10:
+        memory.pop(0)
+
+    for mem in memory:
+        print('>', mem.value1, mem.operator, mem.value2, '=', mem.answer)
+    print('\nContinue your operation'
+          '\nYour first value is ───→', num1)
